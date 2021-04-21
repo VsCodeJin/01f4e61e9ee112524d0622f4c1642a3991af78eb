9a0739b009c8cd02a49d5f03fb427d0695baffa8
Erwin <erwins71297@gmail.com>
2021-04-22 04:43:06 +0700

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Tab from './components/Tab';
 import TabNav from './components/TabNav';
 import Card from './components/Cards';
+import DatePicker from 'react-horizontal-datepicker';
 
 //Home
 class App extends React.Component {
@@ -19,21 +20,37 @@ class App extends React.Component {
   };
 
   render() {
+    const selectedDay = (val) => {
+      console.log(val);
+    };
     return (
       <div className="App mt-4">
-        <TabNav
-          className="tab-item-list"
-          tabs={['Lunch', 'Dinner']}
-          selected={this.state.selected}
-          setSelected={this.setSelected}
-        >
-          <Tab isSelected={this.state.selected === 'Lunch'}>
-            <Card />
-          </Tab>
-          <Tab isSelected={this.state.selected === 'Dinner'}>
-            <Card />
-          </Tab>
-        </TabNav>
+        <div className="Datepicker ">
+          <DatePicker
+            getSelectedDay={selectedDay}
+            endDate={100}
+            selectDate={new Date('2021-04-22')}
+            labelFormat={'MMMM'}
+            color={'#424749'}
+          />
+        </div>
+        <div className="Tabs center">
+          <TabNav
+            className="tab-item-list"
+            tabs={['Lunch', 'Dinner']}
+            selected={this.state.selected}
+            setSelected={this.setSelected}
+          >
+            <Tab isSelected={this.state.selected === 'Lunch'}>
+              <h3 className="Date">Kamis 22 April 2021</h3>
+              <Card />
+            </Tab>
+            <Tab isSelected={this.state.selected === 'Dinner'}>
+              <h3 className="Date2">Kamis 22 April 2021</h3>
+              <Card />
+            </Tab>
+          </TabNav>
+        </div>
       </div>
     );
   }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,21 +7,20 @@ import img4 from '../assets/Bento 2.jpg';
 
 //Cards View
 class Cards extends Component {
-  state = {};
   render() {
     return (
       <div className="container-fluid d-flex justify-content-center">
         <div className="column">
-          <div className="row-md-4">
+          <div className="row-md-4" style={{ width: '375px' }}>
             <Card imgsrc={img1} title="Roasted Chicken with Scrambled Egg" />
           </div>
-          <div className="row-md-4">
+          <div className="row-md-4" style={{ width: '375px' }}>
             <Card imgsrc={img2} title="Fried Broad Noodles" />
           </div>
-          <div className="row-md-4">
+          <div className="row-md-4" style={{ width: '375px' }}>
             <Card imgsrc={img3} title="Japanese Style Bento" />
           </div>
-          <div className="row-md-4">
+          <div className="row-md-4 " style={{ width: '375px' }}>
             <Card imgsrc={img4} title="Japanese Style Bento 2" />
           </div>
         </div>
